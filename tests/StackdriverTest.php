e8cca2a16e5597272c5f94ee32946f33c02cadfe
tomwalder <tom@thinkfluent.co.uk>
2025-09-15 17:23:44 +0100

    Update to support monolog 3.x

--- tests/StackdriverTest.php
+++ tests/StackdriverTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Monolog\Level;
 use PHPUnit\Framework\TestCase;
 use ThinkFluent\RunPHP\Logging\StackdriverJsonFormatter;
 
@@ -8,18 +9,27 @@ class StackdriverTest extends TestCase
     public function testStackdriver()
     {
         $formatter = new StackdriverJsonFormatter(\Monolog\Formatter\JsonFormatter::BATCH_MODE_JSON, true);
-        $messageString = $formatter->format([
-            'context' => [
-                'key1' => 'value1',
-            ],
-            'extra' => [
-                'key2' => 'value2',
-            ],
-            'message' => 'hi',
-            'level_name' => 'warning',
-            'datetime' => new \DateTimeImmutable('2024-01-01 01:02:03'),
-            'channel' => 'channel1',
-        ]);
+        //     public function __construct(
+        //        public readonly \DateTimeImmutable $datetime,
+        //        public readonly string $channel,
+        //        public readonly Level $level,
+        //        public readonly string $message,
+        //        /** @var array<mixed> */
+        //        public readonly array $context = [],
+        //        /** @var array<mixed> */
+        //        public array $extra = [],
+        //        public mixed $formatted = null,
+        //    ) {
+        //    }
+        $record = new \Monolog\LogRecord(
+            new \DateTimeImmutable('2024-01-01 01:02:03'),
+            'channel1',
+            Level::Warning,
+            'hi',
+            ['key1' => 'value1'],
+            ['key2' => 'value2']
+        );
+        $messageString = $formatter->format($record);
 
         $this->assertIsString($messageString);
         $this->assertEquals(PHP_EOL, substr($messageString, -1));
@@ -35,7 +45,7 @@ class StackdriverTest extends TestCase
         $this->assertObjectHasProperty('message', $formatted);
         $this->assertEquals('hi', $formatted->message);
         $this->assertObjectHasProperty('severity', $formatted);
-        $this->assertEquals('warning', $formatted->severity);
+        $this->assertEquals('WARNING', $formatted->severity);
         $this->assertObjectHasProperty('timestamp', $formatted);
         $this->assertObjectHasProperty('seconds', $formatted->timestamp);
         $this->assertEquals(1704070923, $formatted->timestamp->seconds);
